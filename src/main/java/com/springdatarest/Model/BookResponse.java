905c058fa2b20d407fb072a305aa2f3dffe3566f
Umesh Kumar <yumeshkumar6566@gmail.com>
2025-06-19 19:06:34 +0530

    Add BookResponse class its longer needed

---
+++ src/main/java/com/springdatarest/Model/BookResponse.java
@@ -0,0 +1,42 @@
+package com.springdatarest.Model;
+
+
+public class BookResponse {
+
+    private Integer id;
+    private String name;
+    private double price;
+
+    public BookResponse() {}
+
+    public BookResponse(Integer id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
